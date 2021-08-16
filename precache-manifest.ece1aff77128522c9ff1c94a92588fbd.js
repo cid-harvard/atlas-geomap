de32fde077a8e497bb98a8e324a26619e0dc3b76
Kyle Soeltz <wsoeltz@gmail.com>
2021-08-16 16:39:04 -0400

    Updates

---
+++ precache-manifest.ece1aff77128522c9ff1c94a92588fbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73676da291e0db785deb4db7ef952e7e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0238fa26898f3209a989",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "209ca3efed2a0e0e98fb",
+    "url": "./static/js/2.7c887646.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.7c887646.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0238fa26898f3209a989",
+    "url": "./static/js/main.a128c390.chunk.js"
+  },
+  {
+    "revision": "5b199ad1674ef23241b9",
+    "url": "./static/js/runtime-main.ca362f3e.js"
+  }
+]);
